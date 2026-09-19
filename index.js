f172cb40de9d6efa8d687863d07fa199021c040b
shivamgohil16-code <shivamgohil16@gmail.com>
2026-09-19 09:00:33 +0530

    Add keepalive web server

--- index.js
+++ index.js
@@ -101,3 +101,10 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running!');
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Keep-alive server listening on port ${PORT}`));
